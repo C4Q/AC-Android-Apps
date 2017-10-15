eb75bfb48e48a0b2cff397a8e4f3857380adc9a0
Justice Ogbonna <ebesco@yahoo.com>
2017-10-15 12:20:19 -0400

    Programmatically change the displayed text in TextView

--- app/src/main/java/c4q/nyc/androidlayoutsxml/MainActivity.java
+++ app/src/main/java/c4q/nyc/androidlayoutsxml/MainActivity.java
@@ -2,6 +2,7 @@ package c4q.nyc.androidlayoutsxml;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // change the text in the text view
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("I replaced hello world, yay!!!");
     }
 }
