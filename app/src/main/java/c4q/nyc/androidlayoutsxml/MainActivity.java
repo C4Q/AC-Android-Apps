ff4a61033b6e7595f02dc71425c5436d8b5a944c
Justice Ogbonna <ebesco@yahoo.com>
2017-10-15 18:13:16 -0400

    added more attributes to widgets

--- app/src/main/java/c4q/nyc/androidlayoutsxml/MainActivity.java
+++ app/src/main/java/c4q/nyc/androidlayoutsxml/MainActivity.java
@@ -2,9 +2,12 @@ package c4q.nyc.androidlayoutsxml;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +17,7 @@ public class MainActivity extends AppCompatActivity {
         // change the text in the text view
         TextView textView = (TextView) findViewById(R.id.textView);
         textView.setText("I replaced hello world, yay!!!");
+
+        Log.d(TAG, "hello world");
     }
 }
